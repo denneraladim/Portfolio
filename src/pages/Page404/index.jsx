914ce7967798eff846f6cee4df68184cded029f4
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2024-02-10 20:27:22 -0300

    Criado a Responsividade da Página 404 do Max-width: 768px

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -2,12 +2,12 @@ import styles from './Page404.module.scss'
 
 export const Page404 = () => {
   return (
-    <>
+    <section className={styles.page404}>
       <h2 className={styles.titulo2}>Algo de Errado!</h2>
       <div className={styles.textos}>
       <span className={styles.texto_grande}>404</span><br />
       <strong className={styles.texto_vermelho}>Página Não Localizada!</strong>
       </div>
-    </>
+    </section>
   )
 }
