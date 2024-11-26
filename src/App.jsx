2f06572fd9b9d7f88098cbce06293a5de5722dd2
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2024-11-26 15:40:30 -0300

    Criado o componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Footer } from './Components/Footer'
 import { Header } from './Components/Header'
 
 function App() {
     return (
     <>
       <Header />
+      Conteúdo
+      <Footer />
     </>
   )
 }
---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+          Feito por Denner Aladim &copy; 2024
+        </footer>
+    )
+}
