7c4d32c9267da1033f63a768d57f7540008d75e5
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2024-11-28 18:54:16 -0300

    Atualizado a pagina projetos

--- src/Pages/Projetos/index.jsx
+++ src/Pages/Projetos/index.jsx
@@ -1,15 +1,19 @@
 import Card from "../../Components/Card";
-
+import styles from './projetos.module.css'
 
 function Projetos(){
     return (
-        <>
+        <section className={styles.projetos}>
            <h2>Projetos</h2>
-           <Card />
-           <Card />
-           <Card />
+           <section className={styles.lista}>
+               <Card />
+               <Card />
+               <Card />
+               <Card />
+               <Card />
+           </section>
         
-        </>
+        </section>
     )
 }
 
