896d3d65d178f0e5cf3945649889835d41737870
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2024-11-29 20:54:41 -0300

    Atualizado as paginas para a Responsividade

--- src/Pages/Page404/index.jsx
+++ src/Pages/Page404/index.jsx
@@ -3,11 +3,13 @@ import styles from './page404.module.css'
 function Page404(){
     return (
         <>
-            <h2 className={styles.titulo2}>Algo de Errado!</h2>
-            <div className={styles.textos}>
-                <span className={styles.texto_grande}>404</span><br/>
-                <strong className={styles.texto_vermelho}>Página Não Localizada!</strong>
+            <section className={styles.page404}>
+                <h2 className={styles.titulo2}>Algo de Errado!</h2>
+                <div className={styles.textos}>
+                    <span className={styles.texto_grande}>404</span><br/>
+                    <strong className={styles.texto_vermelho}>Página Não Localizada!</strong>
             </div>
+            </section>
          
         </>
     )
